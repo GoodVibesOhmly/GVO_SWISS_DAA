e10ad7487a5552c46eddb2a671f1bbcfa32771c2
geleeroyale <admin@geleeroyale.work>
2020-12-23 00:51:06 +0100

    homescreen visual fixes

--- src/pages/contribute/components/ContributeForm.js
+++ src/pages/contribute/components/ContributeForm.js
@@ -1,139 +1,141 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import './Contribute.sass';
-import DAI from 'cryptocurrency-icons/svg/color/dai.svg';
-import arrow from '../../../assets/arrow.svg';
-import CSTK from '../../../assets/cstk.svg';
-import DonateModal from './DonateModal';
-import { OnboardContext } from '../../../components/OnboardProvider';
+import React from 'react'
+import { connect } from 'react-redux'
+import './Contribute.sass'
+import DAI from 'cryptocurrency-icons/svg/color/dai.svg'
+import arrow from '../../../assets/arrow.svg'
+import CSTK from '../../../assets/cstk.svg'
+import DonateModal from './DonateModal'
+import { OnboardContext } from '../../../components/OnboardProvider'
 
-const config = require('../../../config');
+const config = require('../../../config')
 
 const Comp = ({ onClose }) => {
-  const [amountDAI, setAmountDAI] = React.useState(500);
-  const [amountCSTK, setAmountCSTK] = React.useState(0);
-  const [showDonateModal, setShowDonateModal] = React.useState(false);
+  const [amountDAI, setAmountDAI] = React.useState(500)
+  const [amountCSTK, setAmountCSTK] = React.useState(0)
+  const [showDonateModal, setShowDonateModal] = React.useState(false)
   // const [showThankYouModal, setShowThankYouModal] = React.useState(false);
-  const [donationButtonEnabled, setDonationButtonEnabled] = React.useState(false);
+  const [donationButtonEnabled, setDonationButtonEnabled] = React.useState(
+    false
+  )
 
-  const [DAIError, setDAIError] = React.useState();
+  const [DAIError, setDAIError] = React.useState()
 
   React.useEffect(() => {
     try {
-      const amountDAIFloat = parseFloat(amountDAI);
+      const amountDAIFloat = parseFloat(amountDAI)
       if (Number.isNaN(amountDAIFloat)) {
         if (amountDAI && amountDAI !== '') {
-          setDAIError('please enter a number');
+          setDAIError('please enter a number')
         }
-        setAmountDAI(amountDAIFloat);
-        setAmountCSTK(0);
+        setAmountDAI(amountDAIFloat)
+        setAmountCSTK(0)
       } else {
-        setAmountCSTK(config.ratio * amountDAIFloat);
-        setDAIError(null);
+        setAmountCSTK(config.ratio * amountDAIFloat)
+        setDAIError(null)
       }
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
-  }, [amountDAI]);
+  }, [amountDAI])
 
   React.useEffect(() => {
-    setDonationButtonEnabled(amountCSTK !== 0);
-  }, [amountCSTK]);
+    setDonationButtonEnabled(amountCSTK !== 0)
+  }, [amountCSTK])
 
-  const { isReady } = React.useContext(OnboardContext);
+  const { isReady } = React.useContext(OnboardContext)
 
   return (
     <>
       {showDonateModal && isReady && (
         <DonateModal
           onClose={() => {
-            setShowDonateModal(false);
-            onClose();
+            setShowDonateModal(false)
+            onClose()
             // setShowThankYouModal(true)
           }}
           amount={amountDAI}
         />
       )}
-      <div className="enable has-text-left">
-        <div className="contribmain">
-          <p className="subtitle mb-2">I WANT TO CONTRIBUTE</p>
-          <div className="level">
-            <div className="level-left">
-              <div className="level-item">
-                <div className="field">
-                  <div className="control has-icons-left">
-                    <span className="select">
+      <div className='enable has-text-left'>
+        <div className='contribmain'>
+          <p className='subtitle mb-2'>I WANT TO CONTRIBUTE</p>
+          <div className='level'>
+            <div className='level-left'>
+              <div className='level-item'>
+                <div className='field'>
+                  <div className='control has-icons-left'>
+                    <span className='select'>
                       <select>
                         <option>DAI</option>
                       </select>
                     </span>
-                    <span className="icon is-small is-left">
-                      <figure className="image is-16x16">
-                        <img src={DAI} alt="DAI" />
+                    <span className='icon is-small is-left'>
+                      <figure className='image is-16x16'>
+                        <img src={DAI} alt='DAI' />
                       </figure>
                     </span>
                   </div>
-                  <p className="help is-danger">&nbsp;</p>
+                  <p className='help is-danger'>&nbsp;</p>
                 </div>
               </div>
-              <div className="level-item">
-                <div className="field">
-                  <div className="control">
+              <div className='level-item'>
+                <div className='field' style={{ maxWidth: `100px` }}>
+                  <div className='control'>
                     <input
-                      className="input"
-                      type="number"
-                      placeholder=""
+                      className='input'
+                      type='number'
+                      placeholder=''
                       onChange={e => {
-                        setAmountDAI(e.target.value);
+                        setAmountDAI(e.target.value)
                       }}
                       value={amountDAI}
                     />
                   </div>
-                  <p className="help is-danger">{DAIError || <>&nbsp;</>}</p>
+                  <p className='help is-danger'>{DAIError || <>&nbsp;</>}</p>
                 </div>
               </div>
             </div>
-            <div className="level-item">
-              <div className="field">
-                <div className="control">
+            <div className='level-item'>
+              <div className='field'>
+                <div className='control'>
                   &nbsp;
-                  <img alt="arrow right" src={arrow} />
+                  <img alt='arrow right' src={arrow} />
                   &nbsp;
                   {/* <p class="help is-danger">&nbsp;</p> */}
                 </div>
               </div>
             </div>
-            <div className="level-right">
-              <div className="level-item">
-                <div className="level-item">
-                  <div className="field">
-                    <div className="control">
+            <div className='level-right'>
+              <div className='level-item'>
+                <div className='level-item'>
+                  <div className='field' style={{ maxWidth: `100px` }}>
+                    <div className='control'>
                       <input
-                        className="input"
+                        className='input'
                         disabled
-                        type="text"
+                        type='text'
                         value={amountCSTK}
-                        placeholder=""
+                        placeholder=''
                       />
                     </div>
-                    <p className="help is-danger">&nbsp;</p>
+                    <p className='help is-danger'>&nbsp;</p>
                   </div>
                 </div>
-                <div className="level-item">
-                  <div className="field">
-                    <div className="control has-icons-left">
-                      <span className="select">
+                <div className='level-item'>
+                  <div className='field'>
+                    <div className='control has-icons-left'>
+                      <span className='select'>
                         <select disabled>
                           <option>CSTK</option>
                         </select>
                       </span>
-                      <span className="icon is-small is-left">
-                        <figure className="image is-16x16">
-                          <img src={CSTK} alt="CSTK" />
+                      <span className='icon is-small is-left'>
+                        <figure className='image is-16x16'>
+                          <img src={CSTK} alt='CSTK' />
                         </figure>
                       </span>
                     </div>
-                    <p className="help is-danger">&nbsp;</p>
+                    <p className='help is-danger'>&nbsp;</p>
                   </div>
                 </div>
               </div>
@@ -141,7 +143,7 @@ const Comp = ({ onClose }) => {
           </div>
 
           <button
-            className="button is-success is-fullwidth is-medium"
+            className='button is-success is-fullwidth is-medium'
             disabled={!donationButtonEnabled}
             onClick={() => setShowDonateModal(true)}
           >
@@ -152,8 +154,8 @@ const Comp = ({ onClose }) => {
         {/* </div > */}
       </div>
     </>
-  );
-};
+  )
+}
 
 const mapStateToProps = state => {
   return {
@@ -163,15 +165,15 @@ const mapStateToProps = state => {
     denominator: state.denominator,
     softCap: state.softCap,
     hardCap: state.hardCap,
-    totalReceived: state.totalReceived,
-  };
-};
+    totalReceived: state.totalReceived
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
     onSetAgreedtandc: signature => dispatch({ type: 'AGREE_TANDC', signature }),
-    setShowTandC: value => dispatch({ type: 'SET_SHOW_TANDC', value }),
-  };
-};
+    setShowTandC: value => dispatch({ type: 'SET_SHOW_TANDC', value })
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Comp);
+export default connect(mapStateToProps, mapDispatchToProps)(Comp)
