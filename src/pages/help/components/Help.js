51df0aefaebaf5378263eb2ddda88a2e0dd91205
MerlinEgalite <egalite.merlin@gmail.com>
2021-03-31 21:34:28 +0100

    feat: add faq content

--- src/pages/help/components/Help.js
+++ src/pages/help/components/Help.js
@@ -4,52 +4,70 @@ import { connect } from 'react-redux';
 const Comp = () => {
   return (
     <>
-      <section className="section">
+      <section className="notification is-primary">
         <div className="tile is-horizontal is-parent">
-          <article className="tile is-child notification is-primary">
+          <article className="tile is-child">
             <p className="heading is-size-2 title has-text-weight-bold">FAQ</p>
-            <div>Here you will find the answer to all your questions!</div>
+            <div>Here you will find the answers to all your questions!</div>
           </article>
         </div>
         <div className="tile has-text-left is-horizontal is-parent">
-          <article className="tile is-child notification is-primary">
+          <article className="tile is-child">
             <p className="is-size-4 title has-text-weight-bold">
-              How can I contribute to Commons Stack?
+              What are the legal implications of joining the Trusted Seed within my specific
+              country?
             </p>
             <div>
-              Eodem tempore Serenianus ex duce, cuius ignavia populatam in Phoenice Celsen ante
-              rettulimus, pulsatae maiestatis imperii reus iure postulatus ac lege, incertum qua
-              potuit suffragatione absolvi, aperte convictus familiarem suum cum pileo, quo caput
-              operiebat, incantato vetitis artibus ad templum misisse fatidicum, quaeritatum
-              expresse an ei firmum portenderetur imperium, ut cupiebat, et cunctum.
+              The legal structure of the Trusted Seed is designed for the most litigious legal
+              environments (for example, within the United States). It is designed to protect its
+              members from potential legal situations that could arise from normal good faith
+              actions within DAOs and Commons that are often considered general partnerships with
+              heavy legal liability for individuals.
             </div>
           </article>
         </div>
         <div className="tile has-text-left is-horizontal is-parent">
-          <article className="tile is-child notification is-primary">
+          <article className="tile is-child">
             <p className="is-size-4 title has-text-weight-bold">
-              How can I contribute to Commons Stack?
+              Do I have to report to anyone in my country that I am a member of this association?
             </p>
             <div>
-              Eodem tempore Serenianus ex duce, cuius ignavia populatam in Phoenice Celsen ante
-              rettulimus, pulsatae maiestatis imperii reus iure postulatus ac lege, incertum qua
-              potuit suffragatione absolvi, aperte convictus familiarem suum cum pileo, quo caput
-              operiebat, incantato vetitis artibus ad templum misisse fatidicum, quaeritatum
-              expresse an ei firmum portenderetur imperium, ut cupiebat, et cunctum.
+              No. The legal structure proposed allows the membership to be treated in a similar
+              manner as a membership at your local gym. There should be no reporting requirements
+              for anyone. With that being said, it is always a good idea to keep an eye out for
+              regulatory changes within your local and national jurisdictions.
             </div>
           </article>
         </div>
         <div className="tile has-text-left is-horizontal is-parent">
-          <article className="tile is-child notification is-primary">
+          <article className="tile is-child">
             <p className="is-size-4 title has-text-weight-bold">
-              How can I contribute to Commons Stack?
+              Is it clear where I have breached the Terms & Conditions?
             </p>
             <div>
-              Eodem tempore Serenianus ex duce, cuius ignavia populatam in Phoenice Celsen ante
-              rettulimus, pulsatae maiestatis imperii reus iure postulatus ac lege, incertum qua
-              potuit suffragatione absolvi, aperte convictus familiarem suum cum pileo, quo caput
-              operiebat, incantato vetitis artibus ad templum misisse fatidicum, quaeritatum
-              expresse an ei firmum portenderetur imperium, ut cupiebat, et cunctum.
+              The rules are located in our{' '}
+              <a
+                target="_blank"
+                rel="noreferrer"
+                className="support-link"
+                style={{ color: '#1BDD9D', textDecoration: 'none' }}
+                href="https://ipfs.web3.party/ipfs/QmYDmtmDYUPR6wjukzaNytibeNnYs41s2co4tNzkUYdd5n"
+              >
+                Terms & Conditions
+              </a>{' '}
+              that members must agree to before joining the Swiss Association. In summary, if you
+              act in good faith and do not perform any illegal actions you will be protected.
+            </div>
+          </article>
+        </div>
+        <div className="tile has-text-left is-horizontal is-parent">
+          <article className="tile is-child">
+            <p className="is-size-4 title has-text-weight-bold">
+              Is there a list of what will be covered by the Legal protections?
+            </p>
+            <div>
+              There is not a defined list, but good faith participation in approved DAOs will
+              generally be covered.
             </div>
           </article>
         </div>
