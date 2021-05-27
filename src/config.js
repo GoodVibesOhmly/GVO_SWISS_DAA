4fde81960250828841381078030d133cd187143e
MerlinEgalite <egalite.merlin@gmail.com>
2021-05-27 13:40:32 +0100

    feat(config): remove bridge address

--- src/config.js
+++ src/config.js
@@ -8,7 +8,6 @@ const configs = {
       // rpcEndpoint: "https://ropsten.infura.io/v3/766661aa3a1e414584b9d2c2b73e6930",
       rpcEndpointXdai: 'https://rpc.xdaichain.com/',
     },
-    bridgeAddress: '0x30f938fed5de6e06a9a7cd2ac3517131c317b1e7',
     CSSTKTokenAddress: '0x1A1CCA6c6A436f6B23e4dA99430f00F01688B00d', // XDAI
     DAITokenAddress: '0xE690E380740a682E2b8CEAEa33584Ea2cb59849E', // Ropsten
     registryAddress: '0x28512FB7681c8615aef25a8EF3bcb90aFAC502cB',
@@ -35,7 +34,6 @@ const configs = {
       // rpcEndpoint: "https://mainnet.eth.cloud.ava.do",
       rpcEndpointXdai: 'https://rpc.xdaichain.com/',
     },
-    bridgeAddress: '0x30f938fed5de6e06a9a7cd2ac3517131c317b1e7',
     CSSTKTokenAddress: '0xc4fbE68522ba81a28879763C3eE33e08b13c499E', // XDAI chain
     DAITokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f', // Mainnet
     ratio: 2.5, // how much CSTK you get for 1 DAI
